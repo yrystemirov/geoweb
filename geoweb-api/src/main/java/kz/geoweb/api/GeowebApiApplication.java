e9db7051f40e336ccb37d379076daeb3463c6fe3
yrystemirov <yerhatenu@gmail.com>
2024-08-14 01:15:33 +0500

    Initial commit

---
+++ geoweb-api/src/main/java/kz/geoweb/api/GeowebApiApplication.java
@@ -0,0 +1,13 @@
+package kz.geoweb.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeowebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeowebApiApplication.class, args);
+    }
+
+}
